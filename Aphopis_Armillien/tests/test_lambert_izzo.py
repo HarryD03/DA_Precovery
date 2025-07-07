Fixed Implicit solver DAVec and implimented test found, through implicit solver Davec, Jacobian.inv() does not work for high order DA variables - need to find work around

Aphopis_Armillien/tests/test_lambert_izzo.py
@@ -319,12 +319,14 @@ def test_householder_iter_DA_nom():
 def test_householder_iter_DA_Map_basic():
     """Check mapping sensitivity for x^2=p around p=2."""
     DA.init(4, 2)
+    
     p = 2 + DA(1)
     def f(x):
         return x*x - p
     
     x_nom = householder_iter_DA_nom(1.0, f, 2)
-    x_da = householder_iter_DA_Map(x_nom, 2, f)
+    print(x_nom)
+    x_da = householder_iter_DA_Map(1.0, 2, f)
     assert np.isclose(x_da.cons(), np.sqrt(2), atol=1e-6)
     # derivative dx/dp should be 1/(2*sqrt(2))
     deriv = x_da.deriv(1).cons()

Aphopis_Armillien/tests/test_misc.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from Aphopis_Armillien.utils.iod import lambert_battin_DA, battin_A_DA, battin_x_DA, battin_vel_DA, Implicit_solver_DA, Nf, newton_nomial_DA, kepler_F
+from Aphopis_Armillien.utils.iod import lambert_battin_DA, battin_A_DA, battin_x_DA, battin_vel_DA, Implicit_solver_DA, Nf, newton_nomial_DA, kepler_F, Implicit_solver_DAVec
 from typing import Union
 from daceypy import DA, array
 import daceypy.op as op
@@ -152,21 +152,22 @@ def test_Implicit_solver_DA():
     Test DA scalar - In theory if p is a column vector, it will still work - define func differently.
     """
 
-    DA.init(4,2)
+    DA.init(6,2)
+    p0 = 1.0
+    x0 = 1.0
+    def func(x_da):
+        return x_da**2 - p
 
-    def func(x_da, p):
-        return x_da*x_da - p
-
-    p = 1.0 + DA(1)
-    x_init = DA(1.0) + DA(2)      # deliberately off the root
+    p = p0 + DA(1)
+    x_init = DA(x0)      # deliberately off the root
     root_da = Implicit_solver_DA(x_init, p, func)
     print(f"Solution is:\n{root_da}")
 
-    def ex4_2_1(p0: float):
+    def ex4_2_1(p0: float, x0: float):
         def Nf(x, p):
             return x - (x * x - p) / (2 * x)
         tol = 1e-14
-        x0 = 1.0   # x0 is just some initial guess
+        x0 = x0   # x0 is just some initial guess
         i = 0
 
         # double precision computation => fast
@@ -181,7 +182,7 @@ def test_Implicit_solver_DA():
         p = p0 + DA(1)
         x = x0
         i = 1
-        while i <= 4:
+        while i <= DA.getMaxOrder():
             x = Nf(x, p)
             i *= 2
 
@@ -189,10 +190,64 @@ def test_Implicit_solver_DA():
         print(f"Test: Full DA Newton\n{x}\n{x}\n")
         return (x)
 
-    test_da = ex4_2_1(1.0)
+    test_da = ex4_2_1(p0,x0)
     assert test_da == root_da
     return
 
+def test_Implicit_solver_DAVec():
+    """Root function for testing
+    Basic Test for Independant vector inputs 
+    DA independent inputs
+
+    """
+    DA.init(4, 6) # When greater than 4th order theres an error - Its todo with the .inv() function for inverse jacobian - seems to be an inherent part of DA library
+    p = 1.0 + DA(1)
+    def f(x_da):
+        return x_da**2 - p
+    # Initialize DA variables
+
+    # Set initial guess and parameters
+    x_init = array([1.0, 1.0, 1.0])
+    p = array([1.0 + DA(1), 1.0 + DA(2), 1.0 + DA(3)])
+
+    # Call implicit_solver_DAVec
+    root_da = Implicit_solver_DAVec(x_init, p, f, 3, x0DA=False, DAIOD=False)
+
+    # Check result
+    assert isinstance(root_da, array)
+    assert len(root_da) == 3
+    assert np.allclose(root_da.cons(), [1.0, 1.0, 1.0], atol=1e-6)
+    coeff = root_da[0].getCoefficient([4,0,0,0,0,0])
+    assert np.allclose(coeff, -3.90625e-02, atol=1e-6)
+    # Print the result
+    print(f"Root: {root_da}")
+
+def test_Implicit_solver_DAVecComplex():
+    """Root function for testing
+    Basic Test for Independant vector inputs 
+    DA independent inputs
+    
+    """
+    DA.init(4, 6)  # When greater than 4th order theres an error - Its todo with the .inv() function for inverse jacobian I think
+    def f(x_da):
+        return x_da**2 - p
+    # Initialize DA variables
+
+    # Set initial guess and parameters
+    x_init = array([1.0, 2.0, 3.0])         #Must have a resonable initial guess
+    p = array([1.0 + DA(1), 2.0 + DA(2), 3.0 + DA(3)])
+
+    # Call implicit_solver_DAVec
+    root_da = Implicit_solver_DAVec(x_init, p, f, 3, x0DA=False, DAIOD=False)
+
+    # Check result
+    assert isinstance(root_da, array)
+    assert len(root_da) == 3
+    assert np.allclose(root_da.cons(), [1.0, 1.0, 1.0], atol=1e-6)
+    coeff = root_da[0].getCoefficient([4,0,0,0,0,0])
+    assert np.allclose(coeff, -3.90625e-02, atol=1e-6)
+    # Print the result
+    print(f"Root: {root_da}")
 
 def test_lambert_battin_DA():
     """

Aphopis_Armillien/utils/iod.py
@@ -3,6 +3,7 @@ from typing import Union
 from daceypy import DA, array
 import daceypy.op as op
 from numpy.typing import NDArray
+from scipy.linalg import lu_factor, lu_solve      # or numpy.linalg for tiny systems
 
 def f_g_series(r0, dt, f_order, g_order, mu=1.32712440018e11, v0=None):
     """
@@ -258,8 +259,8 @@ def Guass_8th_seed(pos_obs: Union[NDArray, array], obs_dir: Union[NDArray, array
     #obtain the [r1,r2,r3] [range1,range2,range3]. Rows are the real root, columns are positions
     position = np.array([r_1, r_2, r_3]).T
     ranges = np.array([range_1, range_2, range_3]).T
-
-    return position, ranges
+    range_mag = np.hstack([range_1_mag, range_2_mag, range_3_mag])
+    return position, ranges, range_mag
 
 
 #
@@ -445,25 +446,50 @@ def battin_x_DA(r1_da: Union[array,NDArray], r2_da: Union[array,NDArray], dt: fl
     return x_da
 
 def Nf(x, f, df):
-    k = DA.getMaxVariables()        
+#Newton iteration
+    #x is a scalar value   
+    if isinstance(x, DA):
+        k = DA.getMaxVariables() 
+        f = f.plug(k,0)
+        df = df.plug(k,0)
 
-    f = f.plug(k,0)
-    df = df.plug(k,0)
+        if np.allclose(df, 0):
+            raise ZeroDivisionError("Derivative is zero in Newton iteration")  
+        x1 = x - (f / df)    # f /df 
+        return x1
+    
+    #x is an array x = [x1, x2, x3]
+    #f is the callable function
+    #df is the inverse Jacobian of f wrt x
+    
+    if isinstance(x,array) and isinstance(df[0][0], float):         #DAIOD case
+        step = df @ f.cons()       
+        x1 = x - step
+        return x1
+    
+    if isinstance(x,array) and isinstance(df[0][0], DA):         #Higer Order Map case
+        k = DA.getMaxVariables()
+        NumVar = len(x) 
+        DAVar = k - NumVar + 1
+        for i in range(NumVar):
+            f = f.plug(DAVar + i, 0)
+            for j in range(NumVar):
+                df[i][j] = df[i][j].plug(DAVar + j,0)
 
-    if np.allclose(df, 0):
-        raise ZeroDivisionError("Derivative is zero in Newton iteration")
-    x1 = x - f/df    
-    return x1
+        step = df @ f
+        x1 = x - step
+        return x1
 
-def Implicit_solver_DA(x_da: Union[float,DA], f: callable):
+def Implicit_solver_DA(x_da: Union[float,DA], p, f: callable):
         """
-            X needs to be the last DA variable
-                x_da: Dependant Variable (x_nom + DA(x)) - need to be initalised prior
+            X needs to bethe last DA variable
+                x_da: Dependant Variable 
                 p_da: Independant DA Variables (p_nom + DA(p_da))
-                f: Function for Newtons method
+                f:    Function for Newtons method
             Return
                 x_da as a function of DA(p_da). x_da = x_nom + DA(p_da)
         """
+     
         i = 1
         k = DA.getMaxVariables()
         x_da = x_da + DA(k)
@@ -476,6 +502,56 @@ def Implicit_solver_DA(x_da: Union[float,DA], f: callable):
           i *= 2
 
         x_da = x_da.plug(k,0)
+        return x_da
+
+def Implicit_solver_DAVec(x0: Union[array,NDArray], p, f: callable, NumVariables, x0DA=True, DAIOD=False):
+    """
+    Conduct the Implicit Solver (Newton Iteration) for multi-variables inputs.
+    params: x_da: array of initial variables
+            p: array of dependant variables
+            f: function to solve - i.e. f(x_da; p) = 0 
+            NumVariables: number of variables for Newton iterations
+    return: x_da: root soultion of f(x_da; p) = 0 (array) 
+    """
+    k = DA.getMaxVariables() - NumVariables + 1
+    #Initialise Variables for Automatic differentiation
+    for i in range(NumVariables):
+        x0[i] = x0[i] + DA(k+i)
+    
+    #Calculate the Jacobian of the callable funtion f wrt to the DA variables
+    def Jac(fx):        #Calculate the Jacobian of the Matrix f(x; p). f(x)/DA(1)
+        Jac = np.zeros((NumVariables,NumVariables), dtype=object)
+        for i in range(NumVariables):
+            for j in range(NumVariables):
+                Jac[i][j] = fx[i].deriv(k+j)
+        return array(Jac)                            #Retun Jacobian Matrix (DA)
+    
+    #Newton Iteration - to obtain x = x0 + M(p)
+    iter = 1
+    if DAIOD == True:                       #DAIOD Range specific Newton Iteration (constant inverse Jacobian at x(0))
+        Jac = Jac(f(x0))
+        assert np.linalg.det(Jac.cons()) != 0   #Check that Jacobian is invertible
+        J0inv = Jac.inv()
+
+        while iter <= (DA.getMaxOrder()):
+            x_da = Nf(x0, f(x0), J0inv)
+            iter *= 2
+    else: 
+        while iter <= (DA.getMaxOrder()):   #Higher Order Taylor Map Newton Iteration (Recalculate Inverse Jacobian at each iteration for HOTM solution)
+            x_da = Nf(x0, f(x0), Jac(f(x0)).inv())
+            print(x_da)
+            iter *= 2
+            x0 = x_da
+
+    #Extract the root solution
+
+    if x0DA:        #Return all DA parts in the solution: x = x0 + M(p,x)
+         return x_da
+    
+    else:           #Return  x = x0 + M(p)
+        for i in range(NumVariables):
+            k = DA.getMaxVariables() - NumVariables + i
+            x_da[i] = x_da[i].plug((k+1),0)
         return x_da
 
 def battin_vel_DA(r1: Union[array,NDArray], r2: Union[array,NDArray], a: Union[DA,float], dE: Union[DA,float], dt: float, mu: float) -> DA:
@@ -520,7 +596,7 @@ def lagrange_coefficients(a: Union[float, DA], dE: Union[float, DA], r1: Union[a
 
         return r2, v2
 
-def newton_nomial_DA(x0, p: Union[DA, array, float, NDArray], f: callable, tol: float , MaxIter: float) -> float:
+def newton_nomial_DA(x0: Union[float, NDArray], p: Union[DA, array, float, NDArray], f: callable, tol: float , MaxIter: float) -> float:
     """
     Newtons Method applied to DA to obtain Nomial Solution for dependant variable
     DA must have been initialised Prior

Aphopis_Armillien/utils/time_reference.py
@@ -3,6 +3,9 @@ from typing import Callable, List, Union, overload, Tuple
 from daceypy import DA, array
 import daceypy.op as op
 from numpy.typing import NDArray
+import poliastro as pl
+from astropy.time import Time
+
 
 def J0(y,m,d) -> float:
     """
@@ -60,24 +63,96 @@ def LST(y: float, m: float, d: float, ut: float, EL: float) -> float:
     time_epoch = j0 + UT/24.0
     return lst, time_epoch
 
-def equatorial_to_eclipitcJ2000(dr: float, lst: float, h_e: float) -> float:
+def lst(obs_time):
+    """
+    Calculate Local Sidereal Time (LST) for a given date and location.
+
+    Parameters:
+    y (int or array): Year(s)
+    m (int or array): Month(s)
+    d (int or array): Day(s)
+    ut (float or array): Universal Time(s) in hours
+    EL (float or array): East Longitude(s) in degrees
+
+    Returns:
+    array: Local Sidereal Time(s) in hours
+    """
+    if isinstance(y, np.ndarray):
+        # Ensure all input arrays have the same length
+        assert len(y) == len(m) == len(d) == len(EL)
+
+        # Convert input arrays to numpy arrays
+        y = np.array(y)
+        m = np.array(m)
+        d = np.array(d)
+        EL = np.array(EL)
+
+        # Calculate LST for each input value
+        lsts = np.zeros_like(y)
+        t_observation = np.zeros_like(y)
+        for i in range(len(y)):
+            # Calculate Julian Date
+            JD = J0(y[i], m[i], d[i])
+
+            # Calculate Greenwich Mean Sidereal Time (GMST)
+            GMST = gmst(JD,ut[i])
+
+            # Calculate LST
+            LST = GMST + EL[i] 
+
+            # Store LST value
+            lsts[i] = LST
+            t_observation[i] = JD + ut[i]/24
+
+        return lsts, t_observation
+    else:
+        # Single input value case
+        JD = J0(y, m, d)
+        GMST = gmst(JD, ut)
+        LST = GMST + EL
+        t_observation = JD + ut/24
+        return LST, t_observation
+
+
+def gmst(JD, UT):
+    """
+    Calculate Greenwich Mean Sidereal Time (GMST) from a Julian Date (JD).
+
+    Parameters:
+    JD (float): Julian Date (JD)
+
+    Returns:
+    float: Greenwich Mean Sidereal Time (GMST) in hours
+    """
+    T = (JD - 2451545.0) / 36525.0
+    GMST0 = 100.4606184 + 36000.77004*T + 0.000387933*T**2 - 2.583e-8*T**3   #Greenwich sideral time at 0h UT (degrees)
+    GMST0 = zeroTo360(GMST0)
+
+    GMST = GMST0 + 360.98564736629*(UT)
+
+    return GMST
+
+def equatorial_to_eclipitcJ2000(dr: float, lst: NDArray, h_e: float) -> float:
     """
     Rotate Coordinates from equatorial reference frame to ecliptic reference frame at J2000 epoch.
     :param dr: Perpendicular distance of the observation site from Earth's rotation axis
-    :param lst:
-    :param h_e:
+    :param lst: the local side time of the observing site
+    :param h_e: The height of the observer above the equatorial plane
     :return:
         r_obs: The position vector of the observation station within the ecliptical J2000 reference frame.
     """
-    di = 23.4               #angular displacement of Equator to ecliptic plane (degrees)
-    r_obs_eq = np.array([dr*np.cos(lst), dr*np.sin(lst),h_e]).T
+    di_rad = np.deg2rad(23.4)               #angular displacement of Equator to ecliptic plane (degrees)
+    r_obs_eq = np.array([dr*np.cos(lst), dr*np.sin(lst), np.full_like(lst, h_e)])
+    
     R = np.array([
         [1, 0, 0],
-        [0, np.cos(np.deg2rad(di)), np.sin(np.deg2rad(di))],
-        [0, -np.sin(np.deg2rad(di)), np.cos(np.deg2rad(di))]
+        [0, np.cos(di_rad), np.sin(di_rad)],
+        [0, -np.sin(di_rad), np.cos(di_rad)]
     ])
+    r_obs_ec = np.zeros((3,3))
+    for i in range(0, 3):
+        r_obs_ec[:,i] = R @ r_obs_eq[:,i]
 
-    r_obs_ec = np.dot(R, r_obs_eq)
     return r_obs_ec
 
 def create_da_los_vectors(ra: Union[array, NDArray], dec: Union[array, NDArray]) -> Union[array, NDArray]:
@@ -94,32 +169,39 @@ def create_da_los_vectors(ra: Union[array, NDArray], dec: Union[array, NDArray])
     :return:
         3XN array of LOS vectors. column strucutre: [ρ̂x, ρ̂y, ρ̂z]ᵢ. Row index corresponds to observation instance, and it repreats for N observations
     """
-    assert ra.shape[0] == 1, "RA must be a row vector"
-    assert dec.shape[0] == 1, "DEC must be a row vector"
-    assert ra.shape[1] == dec.shape[1], "Number of RA and DEC observations must match"
+    assert ra.shape == dec.shape, "Number of RA and DEC observations must match"
 
-    num_obs = ra.shape[1]
+    num_obs = len(ra)
     los_vectors = np.empty((3, num_obs), dtype=object)
 
     #DA section
     if isinstance(ra, array):
         for i in range(num_obs):
             los_vectors[:,i] = array([
-                op.cos(dec[0,i]) * op.cos(ra[0,i]),
-                op.cos(dec[0,i]) * op.sin(ra[0,i]),
-                op.sin(dec[0,i])
+                op.cos(dec[i]) * op.cos(ra[i]),
+                op.cos(dec[i]) * op.sin(ra[i]),
+                op.sin(dec[i])
                 ])
 
     #ndarray Section
-    if isinstance(ra[0,0],float):
+    if isinstance(ra[0],float):
         for i in range(num_obs):
             los_vectors[:,i] = np.array([
-                op.cos(dec[0,i]) * op.cos(ra[0,i]),
-                op.cos(dec[0,i]) * op.sin(ra[0,i]),
-                op.sin(dec[0,i])
+                op.cos(dec[i]) * op.cos(ra[i]),
+                op.cos(dec[i]) * op.sin(ra[i]),
+                op.sin(dec[i])
                 ])
     
     else:
         print("Input Error")
 
     return los_vectors
+
+def get_earth_ephemeris(years, months, days, uts):
+    # Create a Time object for each observation
+    times = [Time(f"{year}-{month}-{day} {ut[0]:02d}:{ut[1]:02d}:{ut[2]:02d}", scale='utc') for year, month, day, ut in zip(years, months, days, uts)]
+
+    # Get the ephemeris of Earth for each time
+    earth_ephemeris = [pl.Earth().get_ephemeris(time) for time in times]
+
+    return earth_ephemeris